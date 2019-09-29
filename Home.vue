4c1ec1b001dfd9471f2a3c3a47e3d1c854b91da0
mwklee <55765987+mwklee@users.noreply.github.com>
2019-09-28 22:05:29 -0700

    enhancement
    enhancement

--- Home.vue
+++ Home.vue
@@ -13,6 +13,7 @@
       <b-col id="selectedLocationContainer">
         Selected location:<br/>
         <b-alert show><strong>{{ selectedLocation }}</strong></b-alert>
+        <span v-html="details"></span>
       </b-col>
     </b-row>
   </b-container>
@@ -28,7 +29,13 @@ export default {
   data() {
     return {
       selectedLocation: SELECTPLACEHOLDER,
-      aquifers: AQUIFERS.results // TODO: import aquifers from an API
+      aquifers: AQUIFERS.results, // TODO: import aquifers from an API
+      details: '',
+      fields: {'aquifer_id':'Aquifer Id',
+               'mapping_year':'Mapping Year',
+               'name':'Name',
+               'area':'Area',
+               'vulnerability':'Vulnerability'}
     }
   },
   computed: {
@@ -42,11 +49,10 @@ export default {
             return { value: x, text: x }
           })
           // Sort alphabetical by location
-          // BUG: lowercase items go to bottom of list
           .sort(function(a, b) {
             if (a.value < b.value) { return -1 }
             if (a.value > b.value) { return 1 }
-            return 0
+             return 0
           })
           // Return unique values (must do after sort, refactor if performance issue)
           .filter(function(el, idx, arr) {
@@ -55,6 +61,38 @@ export default {
           // Insert item
           locations.unshift({value: SELECTPLACEHOLDER, text: SELECTPLACEHOLDER, disabled: true })
         return locations
+      }
+  },
+  watch: {
+      selectedLocation:function(val){
+        // Get all records for selected location
+        let locationData = this.aquifers
+          .filter(item => ((item!=null)))  // Filter out null items
+          .filter(item => ((item.location!=null)))  // Filter out items with null location
+          .filter(item => item.location.trim()==val);  // Grab selected location records
+        // Clear variable for template field variable
+        this.details='';
+        // Output table html string initialization
+        let locationDetails = '';
+        locationDetails=locationDetails+'<table border=1>';
+        locationDetails=locationDetails+'<tr>';
+        // Build table header
+        for (var p in this.fields){
+          locationDetails=locationDetails+'<th>'+this.fields[p]+'</th>';
+        }
+        locationDetails=locationDetails+'</tr>';
+        // Loop through all selected records
+        for (var i=0;i<locationData.length;i++){
+          locationDetails=locationDetails+'<tr>';
+          // Output required fields
+          for (p in this.fields){
+            locationDetails=locationDetails+'<td>'+(locationData[i][p]!=null ? locationData[i][p]:'')+'</td>';
+          }
+          locationDetails=locationDetails+'</tr>';
+        }
+        locationDetails=locationDetails+'</table>';
+        // Assign table html string to template field variable
+        this.details=locationDetails;
       }
   }
 };
